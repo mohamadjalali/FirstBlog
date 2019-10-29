13d86cc3bcdab03ead53352946e97c0f9116bc72
mohamadjalali <hawak.slardar@gmail.com>
2019-10-29 23:24:37 +0330

    Initialize admin.login view

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -3,3 +3,10 @@ from . import admin
 @admin.route('/')
 def index():
     return "Hello from admin Index"
+
+@admin.route('/login/')
+def login():
+    session['name'] = 'Alireza'
+    print(session)
+    return "1"
+
