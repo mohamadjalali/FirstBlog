da685446376602177eb455f84f0b843eadeb69b7
mohamadjalali <hawak.slardar@gmail.com>
2019-11-04 01:00:55 +0330

    Use flash messages on 'admin.login'

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,12 +1,32 @@
+from flask import session, render_template, request, abort, flash
+from mod_users.forms import LoginForm
+from mod_users.models import User
 from . import admin
+
 
 @admin.route('/')
 def index():
     return "Hello from admin Index"
 
-@admin.route('/login/')
+
+@admin.route('/login/', methods=['GET', 'POST'])
 def login():
-    session['name'] = 'Alireza'
-    print(session)
-    return "1"
+    form = LoginForm(request.form)
+    if request.method == 'POST':
+        if not form.validate_on_submit():
+            abort(400)
+        user = User.query.filter(User.email.ilike(f'{form.email.data}')).first()
+        if not user:
+            flash("Incorrect Credentials", category='warning')
+            return render_template('admin/login.html', form=form)
+        if not user.check_password(form.password.data):
+            flash("Incorrect Credentials", category='warning')
+            return render_template('admin/login.html', form=form)       
+        session['email'] = user.email
+        session['user_id'] = user.id
+        return "Logged in successfully"
+    if session.get('email') is not None:
+        return "You are already logged in"
+    return render_template('admin/login.html', form=form, title='Admin Login')
+
 
