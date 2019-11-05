abd943e44f45472181a805cc19eeb0c53938cebc
mohamadjalali <hawak.slardar@gmail.com>
2019-11-05 15:49:55 +0330

    Update models.py 'mod_users' folder

--- mod_users/models.py
+++ mod_users/models.py
@@ -1,4 +1,4 @@
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 from sqlalchemy import Column, Integer, String
 
@@ -13,4 +13,5 @@ class User(db.Model):
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
-
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
