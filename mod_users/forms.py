1e15fe2a25569278df14bbaf338d7680e048bf56
mohamadjalali <hawak.slardar@gmail.com>
2019-11-08 00:16:48 +0330

    add RegisterForm to "mod_users.forms"

--- mod_users/forms.py
+++ mod_users/forms.py
@@ -1,10 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms.fields.html5 import EmailField
-from wtforms import PasswordField
+from wtforms import PasswordField, StringField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    email = EmailField(validators=[DataRequired()])
-    password =PasswordField(validators=[DataRequired()])
+    email    = EmailField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired()])
+
+
+class RegisterForm(FlaskForm):
+    email     = EmailField(validators=[DataRequired()])
+    full_name = StringField()
+    password  = PasswordField(validators=[DataRequired()])
+    confirm_password  = PasswordField(validators=[DataRequired()])
 
